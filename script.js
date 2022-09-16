70f493b83d242d87a8f18cfafeff2bfc877f9fc1
jessymatta <jessy.w.matta@gmail.com>
2022-09-16 18:29:45 +0300

    added an eventListener for all cards, that will activate a function when the card is clicked

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
 console.log("working???");
 
+// Select all element with class "card" and store them in cards
+const cards = document.querySelectorAll('.card');
+
+//Adding an event listener that will activate the function flip when a card is clicked
+cards.forEach(card => card.addEventListener('click',flip));
+
+
