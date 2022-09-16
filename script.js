b11782012bf75f8ff1e4818b6c91a52c94af0a0a
jessymatta <jessy.w.matta@gmail.com>
2022-09-16 18:37:34 +0300

    defined the flip function that will toggle the class flip on the clicked element that activated it

--- script.js
+++ script.js
@@ -3,6 +3,12 @@ console.log("working???");
 // Select all element with class "card" and store them in cards
 const cards = document.querySelectorAll('.card');
 
+//A function that will flip the card when its clicked 
+function flip(){
+    this.classList.toggle('flip'); //"this" refers to the element that activated the flip function in the first place
+    console.log(this);
+}
+
 //Adding an event listener that will activate the function flip when a card is clicked
 cards.forEach(card => card.addEventListener('click',flip));
 
